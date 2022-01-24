295f590fafc14f6af3481b3feb8a44c50eb62acf
zhuang ma <zhuangzhimhao@yahoo.com>
2022-01-24 17:25:30 +0800

    modified:   Script01.sql

--- Script01.sql
+++ Script01.sql
@@ -1,7 +1,9 @@
-CREATE TABLE OpenCourses
+CREATE TABLE StudentClub
 (
-	month char(6),
-	course_id char(3) REFERENCES coursemaster(course_id)
+	std_id char(3),
+	club_id char(1),
+	club_name char(6),
+	main_club_flg char(1)
 );
 ALTER TABLE poptbl 
 ADD sex int;
@@ -10,7 +12,30 @@ DROP CONSTRAINT opencourses_pkey;
 ALTER TABLE opencourses 
 RENAME month TO courses_month;
 TRUNCATE coursemaster RESTART IDENTITY;
+UPDATE SomeTable 
+SET p_key = CASE p_key 
+	WHEN 'a' THEN 'c'
+	WHEN 'c' THEN 'a'
+	ELSE p_key 
+	END 
+WHERE p_key IN ('a', 'c');
+INSERT INTO studentclub 
+VALUES 
+('100', '1', '棒球', 'Y'),
+('100', '2', '管弦乐', 'N'),
+('200', '2', '管弦乐', 'N'),
+('200', '3', '羽毛球', 'Y'),
+('200', '4', '足球', 'N'),
+('300', '4', '足球', 'N'),
+('400', '5', '游泳', 'N'),
+('500', '6', '围棋', 'N');
+SELECT *
+FROM pg_catalog.pg_tables 
+WHERE tableowner = 'mz';
+SELECT *
+FROM studentclub;
 
+/*****************************************************************************/
 SELECT CASE pref_name
 	WHEN '�µ�' THEN '�Ĺ�'
 	WHEN '�㴨' THEN '�Ĺ�'
@@ -37,24 +62,7 @@ SELECT pref_name,
 FROM poptbl p 
 GROUP BY pref_name;
 
-UPDATE SomeTable 
-SET p_key = CASE p_key 
-	WHEN 'a' THEN 'c'
-	WHEN 'c' THEN 'a'
-	ELSE p_key 
-	END 
-WHERE p_key IN ('a', 'c');
-
-INSERT INTO opencourses 
-VALUES 
-('200706', '1'),
-('200706', '3'),
-('200706', '4'),
-('200707', '4'),
-('200708', '2'),
-('200708', '4');
-
-
+/*****************************************************************************/
 SELECT course_name,
 	CASE WHEN EXISTS  
 		(
@@ -64,7 +72,7 @@ SELECT course_name,
 		)
 		THEN 'o'
 		ELSE 'x'
-	END AS "6月",
+	END AS "june",
 	CASE WHEN EXISTS  
 		(
 			SELECT course_id
@@ -73,7 +81,7 @@ SELECT course_name,
 		)
 		THEN 'o'
 		ELSE 'x'
-	END AS "7月",
+	END AS "july",
 	CASE WHEN EXISTS  
 		(
 			SELECT course_id
@@ -82,6 +90,40 @@ SELECT course_name,
 		)
 		THEN 'o'
 		ELSE 'x'
-	END AS "8月"
-FROM coursemaster cm
+	END AS "august"
+FROM coursemaster cm;
 
+SELECT course_name, 
+	CASE 
+		WHEN EXISTS
+		(
+			SELECT *
+			FROM opencourses o 
+			WHERE courses_month = '200706' AND course_id = c.course_id 
+		)
+		THEN 'o'
+		ELSE 'x'
+	END AS "june",
+	CASE 
+		WHEN EXISTS
+		(
+			SELECT *
+			FROM opencourses o 
+			WHERE courses_month = '200707' AND course_id = c.course_id 
+		)
+		THEN 'o'
+		ELSE 'x'
+	END AS "july",
+	CASE 
+		WHEN EXISTS
+		(
+			SELECT *
+			FROM opencourses o 
+			WHERE courses_month = '200708' AND course_id = c.course_id 
+		)
+		THEN 'o'
+		ELSE 'x'
+	END AS "august"
+FROM coursemaster c;
+
+/*****************************************************************************/
