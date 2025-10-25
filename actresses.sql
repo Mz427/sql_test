f88618c222a0c3f9df5b33d8c130b8e0fc1600ac
Ma Zhuang <zhuangzhimhao@gmail.com>
2025-10-26 01:26:18 +0800

    modified:   actresses.sql

--- actresses.sql
+++ actresses.sql
@@ -1,4 +1,4 @@
--- Active: 1761285273419@@10.6.7.148@5432@db_mz
+-- Active: 1760379344819@@10.6.7.148@5432@db_mz@public
 CREATE TABLE public.actresses
 (
     name1 VARCHAR(20) PRIMARY KEY,
@@ -18,12 +18,18 @@ CREATE TABLE public.movie_info
 );
 ALTER TABLE movie_info ADD COLUMN theme CHAR(2);
 ALTER TABLE movie_info RENAME COLUMN theme TO movie_theme;
-SELECT *
-FROM actresses LEFT JOIN movie_info;
+DROP TABLE schema_test.table_test;
 SELECT *
 FROM movie_info;
+-- FROM actresses;
+SELECT t1.name1, COUNT(t2.movie_id) AS movie_count
+FROM actresses AS t1 LEFT JOIN movie_info AS t2
+ON t1.name1 = t2.actress
+GROUP BY t1.name1
+HAVING COUNT(t2.movie_id) >= 2;
 INSERT INTO movie_info
 VALUES
+('MDYD-987', '2015-01-10', '涼宮琴音', NULL, NULL),
 ('MDYD-987', '2015-01-10', '涼宮琴音', NULL, NULL);
 UPDATE movie_info
 SET movie_id = 'MIAD-738'
