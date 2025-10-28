a5ddf2e327899e6f44f3d80f6657bd7a1879ec40
Ma Zhuang <zhuangzhimhao@gmail.com>
2025-10-28 11:06:41 +0800

    modified:   actresses.sql

--- actresses.sql
+++ actresses.sql
@@ -20,9 +20,10 @@ ALTER TABLE movie_info ADD COLUMN theme CHAR(2);
 ALTER TABLE movie_info RENAME COLUMN theme TO movie_theme;
 DROP TABLE schema_test.table_test;
 SELECT *
--- FROM movie_info;
+-- FROM movie_info
 FROM actresses
-WHERE name1 LIKE '%天%';
+-- WHERE name1 LIKE '%天%'
+;
 SELECT t1.name1, COUNT(t2.movie_id) AS movie_count
 FROM actresses AS t1 LEFT JOIN movie_info AS t2
 ON t1.name1 = t2.actress
@@ -31,7 +32,12 @@ HAVING COUNT(t2.movie_id) >= 2;
 INSERT INTO movie_info
 VALUES
 ('IPZ-721', '2016-03-26', '天海つばさ', 'magnet:?xt=urn:btih:91487C53DD01282AE2EA98B48C5B53A106C72EB2&size=4977720668&biz=ktr', NULL),
-('SSPD-133', '2017-02-11', '天海つばさ', 'magnet:?xt=urn:btih:9B89923278E7A1218DC593B15A628A75D9001C9E&size=2876746156&biz=ktr', NULL);
+('SSPD-133', '2017-02-11', '天海つばさ', 'magnet:?xt=urn:btih:9B89923278E7A1218DC593B15A628A75D9001C9E&size=2876746156&biz=ktr', NULL)
+;
+INSERT INTO actresses
+VALUES
+('白桃はな', 'Shirato Hana', '白桃花', '2000-05-12', 'JP')
+;
 UPDATE movie_info
 SET movie_id = 'MIAD-738'
 WHERE movie_id = 'MIDA-738';
